eff97316fdd75194ebfffaf23c743971bf1203fc
yuri <krupnik.yuri@gmail.com>
2022-12-22 15:36:26 +0200

    go generic api

--- apps/go/api-rest/main.go
+++ apps/go/api-rest/main.go
@@ -2,104 +2,48 @@ package main
 
 import (
 	"fmt"
-	go_shared "mussia30/libs/go/shared"
-	"net/http"
-	//"github.com/amalshaji/fiber-grpc/proto"
-	"github.com/go-playground/validator/v10"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/gofiber/fiber/v2/middleware/monitor"
 	"github.com/gofiber/fiber/v2/middleware/recover"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	go_fiber_helpers "mussia30/libs/go/fiber-helpers"
-	go_models_user "mussia30/libs/go/models/user"
-	go_mongodb "mussia30/libs/go/mongodb"
+	"mussia30/libs/go/generic-api"
+	"mussia30/libs/go/models/user"
+	go_shared "mussia30/libs/go/shared"
 	//"github.com/yurikrupnik/nx-go-playground/my-lib"
 	"log"
 	// "go.mongodb.org/mongo-driver/bson/primitive"
 )
 
-// type Project struct {
-// 	ID   primitive.ObjectID `json:"id" bson:"_id,omitempty"`
-// 	Name string             `json:"name" bson:"name,omitempty" validate:"required,min=3,max=36"`
-// }
-
-var userCollection = "user"
-
-// var notificationCollection = "notifications"
-type IError struct {
-	Field string `json:"field"`
-	Tag   string `json:"tag"`
-	Value string `json:"value"`
+type Project struct {
+	ID   primitive.ObjectID `json:"id" bson:"_id,omitempty"`
+	Name string             `json:"name" bson:"name,omitempty" validate:"required,min=3,max=36"`
 }
 
-var validate = validator.New()
-
-func ValidateStruct(user go_models_user.User) []*IError {
-	var errors []*IError
-	err := validate.Struct(user)
-	if err != nil {
-		for _, err := range err.(validator.ValidationErrors) {
-			var element IError
-			element.Field = err.StructNamespace()
-			element.Tag = err.Tag()
-			element.Value = err.Param()
-			errors = append(errors, &element)
-		}
-	}
-	return errors
-}
-
-// create validation example
-func create(c *fiber.Ctx) error {
-	var payload go_models_user.User
-	if err := c.BodyParser(&payload); err != nil {
-		return c.Status(http.StatusBadRequest).JSON(err.Error())
-	}
-	errors := ValidateStruct(payload)
-	if errors != nil {
-		return c.Status(fiber.StatusBadRequest).JSON(errors)
-	}
-	return c.JSON(fiber.Map{
-		"message": "Hello World",
-	})
-}
+var db = "goApp"
+var userCollection = "users"
+var projectsCollection = "projects"
 
 func main() {
-	// Connect to the database
-	//conn, err := grpc.Dial("localhost:4040", grpc.WithInsecure())
-	//if err != nil {
-	//	panic(err)
-	//}
-	//
-	//client := proto.NewAddServiceClient(conn)
-	if err := go_mongodb.Connect(); err != nil {
-		log.Println("failed to connect")
-		log.Fatal(err)
-	}
-
 	app := fiber.New(fiber.Config{
 		ErrorHandler: go_fiber_helpers.DefaultErrorHandler,
 	})
 
 	app.Use(recover.New())
-	// Default middleware config
 	app.Use(logger.New())
 	//app.Use(csrf.New()) // todo check it - forbidden post events
 	// todo cors in prod!
 	app.Use(cors.New())
 	apiGroup := app.Group("api")
-	apiGroup1 := app.Group("v1")
-	apiGroup1.Get("/aris", func(ctx *fiber.Ctx) error {
-		return ctx.SendString("no51111")
-	})
-	apiGroup1.Post("/friends", create)
-	apiGroup1.Get("/sap", func(ctx *fiber.Ctx) error {
-		return ctx.SendString("bir!!")
-	})
+	//	users.New[users.User](apiGroup, db, userCollection)
+	go_generic_api.New[go_models_user.User](apiGroup, db, userCollection)
+	go_generic_api.New[Project](apiGroup, db, projectsCollection)
+
 	//go_models_user.CreateFakeGroup[users.User](apiGroup, "users")
 	// go_fiber_helpers.CreateFakeGroup[Project](apiGroup, "projects", "projects")
-	go_fiber_helpers.CreateFakeGroup[go_models_user.User](apiGroup, "users", userCollection)
+	//go_fiber_helpers.CreateFakeGroup[go_models_user.User](apiGroup, "users", userCollection)
 
 	app.Get("/dashboard", monitor.New())
 	port := go_shared.Getenv("PORT", "8080")
--- libs/go/models/user/go-models-user.go
+++ libs/go/models/user/go-models-user.go
@@ -1,13 +1,9 @@
 package go_models_user
 
-// User omitempty does not save in db if does not exists!
 type User struct {
 	ID        string `json:"id,omitempty" bson:"_id,omitempty"`
 	Email     string `validate:"required,email,min=6,max=32" json:"email,omitempty" bson:"email,omitempty"`
 	Username  string `validate:"required" json:"username" bson:"username"`
-	FirstName string `validate:"required" json:"first_name" bson:"first_name,omitempty"`
-	LastName  string `validate:"required" json:"last_name" bson:"last_name,omitempty"`
-	//Role  string `json:"role"`
-	//Role float64 `json:"salary"`
-	//Age    float64 `json:"age"`
+	FirstName string `validate:"required" json:"firstName" bson:"first_name,omitempty"`
+	LastName  string `validate:"required" json:"lastName" bson:"last_name,omitempty"`
 }
