3814b864e69287568047f1b027dfc057e6042f7e
yuri <krupnik.yuri@gmail.com>
2022-12-27 23:36:49 +0200

    grpc node

--- apps/node/users-grpc/src/app/app.service.ts
+++ apps/node/users-grpc/src/app/app.service.ts
@@ -1,8 +1,34 @@
 import { Injectable } from '@nestjs/common';
+import { Connection, FilterQuery, Model, QueryOptions } from 'mongoose';
+import { User, UserDocument } from '@mussia30/node/nest/users-api';
+import { handleError } from '@mussia30/node/nest/errors';
+import { InjectConnection, InjectModel } from '@nestjs/mongoose';
 
 @Injectable()
 export class AppService {
-  getData(): { message: string } {
-    return { message: 'Welcome to users-grpc!' };
+  constructor(
+    @InjectModel(User.name) private model: Model<UserDocument>,
+    @InjectConnection() private connection: Connection // private logger: Logger
+  ) {}
+
+  findAll(
+    query: FilterQuery<UserDocument>,
+    projection: any,
+    config: QueryOptions
+  ) {
+    return this.model.find(query, projection, config).lean().catch(handleError);
+  }
+
+  findById(id: string, projection: any) {
+    return this.model.findById(id, projection);
+  }
+
+  create(createEntityData: Partial<User>): Promise<User> {
+    const entity = new this.model(createEntityData);
+    return entity.save().catch(handleError);
+  }
+
+  delete(id: string) {
+    return this.model.findByIdAndDelete(id);
   }
 }
--- apps/node/users-grpc/src/main.ts
+++ apps/node/users-grpc/src/main.ts
@@ -1,22 +1,26 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
-import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-
 import { AppModule } from './app/app.module';
+import { Logger } from '@nestjs/common';
+import { Transport } from '@nestjs/microservices';
+import { join } from 'path';
+
+const logger = new Logger('Main');
+
+const microserviceOptions = {
+  transport: Transport.GRPC,
+  options: {
+    package: 'app',
+    protoPath: join(process.cwd(), '_proto/users.proto'),
+  },
+};
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3333;
-  await app.listen(port);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+  const app = await NestFactory.createMicroservice(
+    AppModule,
+    microserviceOptions
   );
+  app.listen().then(() => {
+    logger.log('Microservice is listening...');
+  });
 }
-
 bootstrap();
