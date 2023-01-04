1b6b44ebe8251b0fd552022bc65297046168a1dc
yuri <krupnik.yuri@gmail.com>
2023-01-04 22:21:21 +0200

    update check

--- apps/frontend/react-app/src/app/routes/users/users.tsx
+++ apps/frontend/react-app/src/app/routes/users/users.tsx
@@ -6,8 +6,8 @@ import ThumbDownIcon from '@mui/icons-material/ThumbDown';
 import SkipNextIcon from '@mui/icons-material/SkipNext';
 import PauseIcon from '@mui/icons-material/Pause';
 import axios from 'axios';
-import { useMachine } from '@xstate/react';
-import { createMachine, assign, raise } from 'xstate';
+import { useMachine, useInterpret, useActor } from '@xstate/react';
+import { createMachine, assign, raise, spawn, send } from 'xstate';
 import { User } from '@mussia30/node/nest/users-api';
 // import { Create§ } from '@xstate';
 // import toggleMachine from '../../xstate';
@@ -21,17 +21,30 @@ import { useQuery, useMutation } from 'react-query';
 import { useEffect, useState } from 'react';
 
 // todo check import wtf!
-import { doit } from '@mussia30/node/state-machines';
+import {
+  doit,
+  promiseMachine,
+  fetchMachine as Mac,
+} from '@mussia30/node/state-machines';
 
 function getUsers(q: any) {
+  console.log('q getUsers', q);
+
   return axios.get<User[]>('http://localhost:8080/api/users').then((res) => {
-    // console.log({ data: res.data });
+    console.log({ data: res.data });
     return res.data;
   });
 }
 function getUser(id: string, q: any) {
-  return axios.get(`http://localhost:8080/api/users/${id}`).then((res) => {
-    // console.log({ data: res.data });
+  return axios
+    .get<User>(`http://localhost:8080/api/users/${id}`)
+    .then((res) => {
+      // console.log({ data: res.data });
+      return res.data;
+    });
+}
+function deleteUser(id: string, q: any) {
+  return axios.delete(`http://localhost:8080/api/users/${id}`).then((res) => {
     return res.data;
   });
 }
@@ -46,108 +59,200 @@ function createUser(data: any) {
 //   data: undefined,
 // };
 const fetchMachine =
-  /** @xstate-layout N4IgpgJg5mDOIC5QDMwBcDGALAdASwgBswBiAMQFEAVAYQAkBtABgF1FQAHAe1jzTy4A7diAAeiJgBoQATwkBfedNSZcyAIZ5CAVwBOpAErUDATWZskIbr35CR4hFNkLF0wVwhwRK7COt8BYUsHAFoAFgAOaTkEEIiAdhxIgFYANnjkiIBGLPj0zNcQH1wCYj8eALtgiWjEACYwpkLinEIudQg8QShym0D7GucEAGZ4sKSEsJywgE4ZpjrhsLDm9GwcWG0MDDh4S39bINAHXJws4Yj0rITk2oRkrNSkrLqZ1Iurm9XVHA0tPTAvUqRzEgxiqTCwySqTqi2WM3iTERw0UiiAA */
-  createMachine({
-    id: 'fetch',
-    initial: 'idle',
-    context: {
-      userId: 0,
-      user: undefined,
-      error: undefined,
-    },
-    states: {
-      idle: {
-        on: {
-          FETCH: { target: 'loading' },
-        },
+  /** @xstate-layout N4IgpgJg5mDOIC5QDMwBcDGALAdASwgBswBiAMQFEAVAYQAkBtABgF1FQAHAe1jzTy4A7diAAeiALQBGJgGYcAdgAsATgUBWWaqnqAHDNkAaEAE9JUgGwqcS2SoBMKqRosX1K1wF9Px1JlwExCRoAIYATjBozGxIINy8-EIi4gjScoqqGlpOegbGZggKOLKyTBa6SrYV6lIV3r7o2DiwYMQYaJA4fth4glAAMniwaCQQQmD4ggBuXADWE5EAqi1hsNEi8XwCwrEpltYqWhYyrgoOrvb5iFL2OEwqena6ulqO+rr1IN24LW0dEF1GlhegMhiMwGEwlwwjgOIQQmhkNCALY4JYrNasDY8LZJXbXKw4Q5KY5lCxnewXK4IGRKHC6M6WKQqFRMe66FRKbw+ECCLgQOAib7YhLbZKSeyydQ4VxyEkqCq6Cyyey6akSezqCxE+yqclair2BSSz7ffBEMAi3E7UApaQMmUWOVWRXK1XUqQ2JjqI1lBS6MpOWwKU1AnD8ZGQADyAFc0FbEjaxOYHbKjgqlEqVWrTNdasUA-cFDolExM14eWaBcQOssIQmxfjUrUimn5a7s9T3PTzi8nE69CpQ-5mq0wO1IA28bbJEpbkXVEoFGdZJZ7kp1RoCwqzmWTqqQ5Ww79x-9Af4QYNhlOkylpazNd77PYyn2ytTdTh1Gz-U6mApV2NWRhyaE8JwBQguBCAUIBvcUEBKaVZA0KQ50lJQpBVJR1GpEoMmQzlAIUKx1BAn4x3AroQjwYhYNiTZE3gxDihQtCtEw3UcNzGlKS-I0X0qVdlTUQ8GhHSDoJBOCm10eklwfZlng5Bl1UsWSHiYTDXEqMsMO5TwgA */
+  createMachine(
+    {
+      id: 'fetch',
+      initial: 'selected',
+      type: 'parallel',
+      context: {
+        userId: 0,
+        user: undefined,
+        error: undefined,
+        data: [],
       },
-      loading: {
-        invoke: {
-          // id: 'getUsers',
-          // src: (context, event) => getUsers({}),
-          onDone: {
-            target: 'success',
-            actions: assign({ user: (context, evnt) => event.data }),
+      states: {
+        idle: {
+          on: {
+            FETCH: { target: 'loading' },
+            target: 'selected',
           },
-          onError: {
-            target: 'failure',
-            actions: assign({ error: (context, event) => event.data }),
+          states: {},
+        },
+        timedOut: {
+          meta: {
+            error: 'Timeout error!',
           },
+        },
+        deleteUser: {},
+        selected: {
+          initial: 'fetchingList',
           states: {
-            success: {},
-            failure: {},
+            fetchingList: {
+              // entry: {},
+              invoke: {
+                id: 'getUsers',
+                src: getUsers,
+                // onDone: 'loaded',
+                after: {
+                  5000: 'timedOut',
+                },
+                onDone: {
+                  target: 'loaded',
+                  actions: assign({
+                    data: (context, event) => {
+                      return event.data;
+                    },
+                  }),
+                },
+                onError: {
+                  target: 'failed',
+                  actions: assign({
+                    data: (context, event) => {
+                      alert(JSON.stringify(context, null, 2));
+                      return event.data;
+                    },
+                  }),
+                },
+              },
+            },
+            loaded: {},
+            failed: {
+              on: {
+                // "Event 1": "loading"
+              },
+            },
           },
         },
+        // selected: {
+        //   invoke: {
+        //     id: 'getUsers',
+        //     src: getUsers,
+        //     onDone: 'loaded',
+        //     onError: 'failed'
+        //   }
+        // },
+        // loaded: {},
+        loading: {},
+        // failed: {}
       },
-      success: {},
-      failure: {
-        on: {
-          RETRY: { target: 'loading' },
-        },
-      },
-    },
-  });
 
-const promiseMachine = {
-  id: 'promise',
-  initial: 'pending',
-  states: {
-    pending: {
-      on: {
-        RESOLVE: { target: 'resolved' },
-        REJECT: { target: 'rejected' },
+      // success: {},
+      // failure: {
+      //   on: {
+      //     RETRY: { target: 'loading' },
+      //   },
+      // },
+      // loading: {
+      //   invoke: {
+      //     id: 'getUsers',
+      //     src: 'getUsers',
+      //     // src: (context, event) => getUsers({}),
+      //     onDone: {
+      //       target: 'idle',
+      //       // actions: assign({ user: (context, evnt) => event.data }),
+      //     },
+      //     // onError: {
+      //     //   target: 'failure',
+      //     //   actions: assign({ error: (context, event) => event.data }),
+      //     // },
+      //     states: {
+      //       success: {},
+      //       failure: {},
+      //     },
+      //   },
+      // }
+      // },
+    },
+    {
+      actions: {
+        getUsers,
       },
-    },
-    resolved: {
-      type: 'final',
-    },
-    rejected: {
-      type: 'final',
-    },
-  },
-};
+    }
+  );
 
-const m1 = createMachine(promiseMachine, {});
+// const promiseMachine =
+// /** @xstate-layout N4IgpgJg5mDOIC5QAcBOB7AtgS1mAdMmAHYTbFQDEASgKIDKA8gDIBqtA2gAwC6iK6WNgAu2dMX4gAHogC0AVgAs+AIzyAbJpUBOABwB2AMxdF67QBoQATzkAmFfn3qne2we3rDu5wF8fltCxcAiJScio6AClaAGEAFW4+JBBkQRExCWSZBAVlNU11HQNjUwtrRENtfHt5Q31bLl15eX0VW20-fxBidAg4SUCcPAG00XFJbNlbRS58XS5DeV02uv1tdv1LGxyVFVmuNq4lRUVDRe0vPwCMIYIAMzBhAGMACxGhMczQSZ1bOYWlit9GsNltEGpdPhDDN1ioZjpgUorikbsFCCQyBR3ulxlk5CpnPhFCpdF4uEYjCZNuUdspvJV5oVdupaupkYM0ag4OgADYAN0g2M+E0QTihwJJHkM6lsJWp2ymf3ktk8+npam0KjW+nZqLw+C5ACswE9hILkqkPhkRQgxatJZ4ZXKwQgvESNGYtcTtKdpZ0fEA */
+// createMachine({
+//   id: 'promise',
+//   initial: 'pending',
+//   states: {
+//     // fetch: {
+//     //   invoke: {
+//     //     id: "fetchMachine",
+//     //     src: fetchMachine,
+//     //   }
+//     // },
+//     // video: {
+//     //   invoke: {
+//     //     id: 'fetchMachine',
+//     //     src: fetchMachine,
+//     //   },
+//     // },
+//     pending: {
+//       on: {
+//         RESOLVE: { target: 'resolved' },
+//         REJECT: { target: 'rejected' },
+//       },
+//     },
+//     resolved: {
+//       type: 'final',
+//     },
+//     rejected: {
+//       type: 'final',
+//     },
+//   },
+// });
+
+// const m1 = createMachine(promiseMachine, {});
 
 const Users = () => {
-  // eslint-disable-next-line react-hooks/rules-of-hooks
   // const [localData, setLocalData] = useState([]);
-  const [state, send] = useMachine(fetchMachine, {
-    // actions: {
-    //   load: () => {
-    //     getUsers({}).then((res) => {
-    //       console.log('res', res);
+  // const [state, send] = useMachine(fetchMachine, {
+  const actor = useInterpret(promiseMachine, {
+    actions: {
+      // load: () => {
+      getUsers,
+      // getUsers: () => getUsers({}).then((res) => {
+      //   console.log('res', res);
 
-    //       send({ type: 'RESOLVE', data: res });
-    //     });
-    //   },
-    // },
-    devTools: true,
+      //   send({ type: 'RESOLVE', data: res });
+      // })
+      // },
+    },
+    // devTools: true,
   });
-  console.log('state', state);
+  const [state, send] = useActor(actor);
+  // const isLoading = state.matches('loading');
+  // console.log('state', state);
+  // console.log('state.context', state.context);
   // Queries
   // const { data } = useQuery(['users', {}], getUsers);
-  // const query = useQuery({ queryKey: ['todos'], queryFn: getTodos })
-  // console.log('state', state);
-  // Mutations
-  // const mutation = useMutation({
-  //   mutationFn: createUser,
-  //   onSuccess: (queryClient, a, b) => {
-  //     // Invalidate and refetch
-  //     console.log('queryClient', queryClient);
-  //     console.log('TVariables', a);
-  //     console.log('TContenxt | undefined', b);
-  //     // queryClient.invalidateQueries({ queryKey: ['users'] });
+  // const { data } = useQuery({
+  //   queryKey: ['users'],
+  //   queryFn: getUsers,
+  //   initialData: [],
+  //   onSuccess: (a, v, c) => {
+  //     console.log('a, b', a, v, c);
+  //     // todo send data to xstate
+  //     // send('getUsers.onDone', a);
+  //     // send('SET_DATA', a);
+  //     // return [{}];
   //   },
   // });
+  // console.log('state', state);
+  // Mutations
+  const mutation = useMutation({
+    mutationFn: createUser,
+    onSuccess: (queryClient, a, b) => {
+      // Invalidate and refetch
+      console.log('queryClient', queryClient);
+      console.log('TVariables', a);
+      console.log('TContenxt | undefined', b);
+      // queryClient.invalidateQueries({ queryKey: ['users'] });
+    },
+  });
 
-  // useEffect(() => {
-  // getUsers(1).then((res) => {
-  //   console.log('res', res);
-  //   setLocalData(res);
-  // });
-  // }, []);
-  // console.log('data', data);
   return (
     <div>
       This is the generated root route.{' '}
@@ -157,11 +262,8 @@ const Users = () => {
           ? 'Click to activate'
           : 'Active! Click to deactivate'}
       </button>
-      {/*<button onClick={() => send('DOIT')}>DOIT command</button>*/}
-      {/*<div>count: {state.context.count}</div>*/}
-      <div>dam all users here</div>
       <div>
-        {/*<div>{state.context.count}</div>*/}
+        <div>{state.context.count}</div>
         {/*<output>{state.context.duration - state.context.elaspsed}</output>*/}
         <Button onClick={() => send({ type: 'FAVE' })}>
           <ThumbUpAltIcon />
@@ -210,11 +312,46 @@ const Users = () => {
       >
         Add user
       </Button>
-      {/*<p>{JSON.stringify(data, null, 2)}</p>*/}
-      {/*{Array.isArray(state.context.users) &&*/}
-      {/*  state.context.users.map((item) => {*/}
-      {/*    return <div key={item._id}>{item._id}</div>;*/}
-      {/*  })}*/}
+      {/* <p>{JSON.stringify(data, null, 2)}</p> */}
+      <h2>Data variable</h2>
+      {/*{data.map((item) => {*/}
+      {/*  return (*/}
+      {/*    <div key={item._id}>*/}
+      {/*      <div>da</div>*/}
+      {/*      <div>{item.email}</div>*/}
+      {/*      <Button*/}
+      {/*        onClick={() => {*/}
+      {/*          send('deleteUser', {*/}
+      {/*            id: item._id,*/}
+      {/*          });*/}
+      {/*          // send('deleteUser', {*/}
+      {/*          //   id: item._id,*/}
+      {/*          // });*/}
+      {/*        }}*/}
+      {/*      >*/}
+      {/*        Delete*/}
+      {/*      </Button>*/}
+      {/*    </div>*/}
+      {/*  );*/}
+      {/*})}*/}
+      <h2>Context Data variable</h2>
+      {state.context.data.map((item) => {
+        return (
+          <div key={item._id}>
+            <div>da2</div>
+            <div>{item.email}</div>
+            <Button
+              onClick={() =>
+                send('deleteUser', {
+                  id: item._id,
+                })
+              }
+            >
+              Delete
+            </Button>
+          </div>
+        );
+      })}
     </div>
   );
 };
--- libs/node/state-machines/src/lib/node-state-machines.ts
+++ libs/node/state-machines/src/lib/node-state-machines.ts
@@ -1,66 +1,235 @@
-// import { assign, createMachine } from 'xstate';
+import { assign, createMachine } from 'xstate';
+import axios from 'axios';
+import { User } from '@mussia30/node/nest/users-api';
 export function doit() {
   return 'do it';
 }
 
-export const promiseMachine = {
-  id: 'promise',
-  initial: 'pending',
-  states: {
-    pending: {
-      on: {
-        RESOLVE: { target: 'resolved' },
-        REJECT: { target: 'rejected' },
-      },
-    },
-    resolved: {
-      type: 'final',
-    },
-    rejected: {
-      type: 'final',
-    },
-  },
+function getUsers(q: any) {
+  console.log('q getUsers', q);
+
+  return axios.get<User[]>('http://localhost:8080/api/users').then((res) => {
+    console.log({ data: res.data });
+    return res.data;
+  });
+}
+function getUser(id: string, q: any) {
+  return axios
+    .get<User>(`http://localhost:8080/api/users/${id}`)
+    .then((res) => {
+      // console.log({ data: res.data });
+      return res.data;
+    });
+}
+function deleteUser(id: string, q: any) {
+  return axios.delete(`http://localhost:8080/api/users/${id}`).then((res) => {
+    return res.data;
+  });
+}
+function createUser(data: any) {
+  return axios.post('http://localhost:8080/api/users', data).then((res) => {
+    console.log('createUsers', { data: res.data });
+    return res.data;
+  });
+}
+
+type PromiseMachineContext = {
+  data: Array<any>;
+  count: number;
 };
 
-// export const fetchMachine =
-//   /** @xstate-layout N4IgpgJg5mDOIC5QDMwBcDGALAdASwgBswBiAMQFEAVAYQAkBtABgF1FQAHAe1jzTy4A7diAAeiJgBoQATwkBfedNSZcyAIZ5CAVwBOpAErUDATWZskIbr35CR4hFNkLF0wVwhwRK7COt8BYUsHAFoAFgAOaTkEEIiAdhxIgFYANnjkiIBGLPj0zNcQH1wCYj8eALtgiWjEACYwpkLinEIudQg8QShym0D7GucEAGZ4sKSEsJywgE4ZpjrhsLDm9GwcWG0MDDh4S39bINAHXJws4Yj0rITk2oRkrNSkrLqZ1Iurm9XVHA0tPTAvUqRzEgxiqTCwySqTqi2WM3iTERw0UiiAA */
-//   createMachine({
-//     id: 'fetch',
-//     initial: 'idle',
-//     context: {
-//       userId: 42,
-//       user: undefined,
-//       error: undefined,
-//     },
-//     states: {
-//       idle: {
-//         on: {
-//           FETCH: { target: 'loading' },
-//         },
-//       },
-//       loading: {
-//         invoke: {
-//           // id: 'getUsers',
-//           // src: (context, event) => getUsers({}),
-//           onDone: {
-//             target: 'success',
-//             actions: assign({ user: (context, event) => event.data }),
-//           },
-//           onError: {
-//             target: 'failure',
-//             actions: assign({ error: (context, event) => event.data }),
-//           },
-//           states: {
-//             success: {},
-//             failure: {},
-//           },
-//         },
-//       },
-//       success: {},
-//       failure: {
-//         on: {
-//           RETRY: { target: 'loading' },
-//         },
-//       },
-//     },
-//   });
+export const promiseMachine = 
+/** @xstate-layout N4IgpgJg5mDOIC5QAcBOB7AtgS1mAdAIYDGALtgG5gDEAKgPIDijAMgKIDaADALqIrpY2cugB2-EAA9EAJgAsAZnwA2AIzKA7HLkBOHQFZVqjTIA0IAJ6IAHKvxzr+5csWbN+jdYC+X82iy4BNiiJORUdEysnLwSyILC2GIS0ggyqjr4GqoyzsaK6nJcyuZWCKoKdgblWlz6Cgb6XN6+IP44ePjIYKIQwVDUAEpsAMr0LABq0XxIrfEi4jMpaRlZOWpaFS5FJbIK+pkKygpcOrWqhocaPn4Y7QRdPX2DbABSbADCtNzTAkLzybJ0plsrkNgVtpZEKprDJ7MYmgoajIFIpmjcAh1YIQqABVPCoajDACCkwA+jjhmwBt9YnNEgtQEsgatQfktsVIalrMp8DIuHzoRpERodDIdFcWm1AvgAGZgUjEAAWfRYuFIAElSGBMNQIGIgqIKOgANYEGCkPFgVA0mZxP70gGpRoqMUeDRHTSC-Q7BAmOT4BQwsU6OTItKea6tW7SuUK5WiKCq2CkXX6-DBI2m-Dmy2oWA234JJKLRDOj1FHIyPkaLgVH0o-bGRwihTHbSKOSRqUdWNKlVqzXa6hWjCoToAG0IpBl6FQmGz8tzBdm9uLjKhanstnkyhDMNDZk5bt51gqOVPqlqcgl6LusvlfYTSZTI9nE6nM7nC4t+PzMVtdJrlIiAALRCj6+j6LCB5FIGMgaIYcjKD4LSiOgEBwLE0Z4LSq4MsBCAgdkqg+iBPJcBRXBGO2JzGOKXbYQQoSUGAuFFvhKSXPg3LWFwcjpKKXo+qc+C1MofHWFk1EnMhkqMemIRkCxbH-CWZS1vg+hyIY8E8coUEoj6kmaU4ElSaohQGAxGL3N0vQJipDpqZedjaScoZ6JshQcqU0IZAofK8RZ8IBTo1l3qgcDoOOVAQI5QGcZo3HKLx-F6Gkknepy8iwi4FlChoNbBbJt7SpFABWYBkJA8UcYgXE8XxAkZQhPrQnY+nWIUrY6M4RTaeF0pYri+K1Y6orJalvVpPoMK1j6MjcioOhGJoF6hicg09g+8aJgOWqYGNanXj6Lg8nyWkmCGaiaFtBC9rtz5HeuCAONxhTwScvUnIiWWlNosK9QJ4meLo2R3fgohgAA7gABMmU5gLDqjPQRrZcCo+koqGcHbn9paFAG3KhtYp7HFwEYoUAA */
+createMachine<PromiseMachineContext>(
+  {
+    id: 'promise',
+    initial: "fetchingList",
+    context: {
+      data: [],
+      count: 0,
+      // login,
+      // logout
+    },
+    states: {
+      active: {
+        // after: "pending",
+        on: {
+          TOGGLE: { target: 'inactive' },
+        },
+      },
+
+      inactive: {
+        on: {
+          TOGGLE: { target: 'active' },
+        },
+      },
+
+      pending: {
+        on: {
+          RESOLVE: { target: 'resolved' },
+          REJECT: { target: 'rejected' },
+        },
+      },
+
+      resolved: {
+        type: 'final',
+      },
+
+      rejected: {
+        type: 'final',
+      },
+
+      saveUser: {
+        on: {
+          SAVE_USER: { target: 'resolved' },
+        },
+      },
+
+      fetchingListItem: {
+        // entry: {},
+        invoke: {
+          id: 'getUser',
+          src: "getUser",
+
+          // onDone: 'loaded',
+          // after: {
+          //   5000: "timedOut",
+          // },
+          // onError: {
+          //   target: 'failed',
+          //   actions: assign({
+          //     data: (context, event) => {
+          //       alert(JSON.stringify(context, null, 2));
+          //       return event.data;
+          //     },
+          //   }),
+          // },
+          onDone: {
+            // target: 'loaded',
+            actions: assign({
+              data: (context, event) => {
+                console.log('fetchingListItem context', context, event);
+                return event.data;
+              },
+            }),
+          },
+
+          onError: "new state 1"
+        },
+      },
+
+      fetchingList: {
+        // entry: {},
+        invoke: {
+          id: 'getUsers',
+          src: "getUsers",
+
+          // onDone: 'loaded',
+          // after: {
+          //   5000: "timedOut",
+          // },
+          // onError: {
+          //   target: 'failed',
+          //   actions: assign({
+          //     data: (context, event) => {
+          //       alert(JSON.stringify(context, null, 2));
+          //       return event.data;
+          //     },
+          //   }),
+          // },
+          onDone: {
+            // target: 'loaded',
+            actions: assign({
+              data: (context, event) => {
+                console.log('context', context, event);
+                return event.data;
+              },
+            }),
+          },
+
+          onError: "fetchingListItem"
+        },
+      },
+
+      "new state 1": {}
+    },
+  },
+  {}
+);
+
+export const fetchMachine =
+  /** @xstate-layout N4IgpgJg5mDOIC5QDMwBcDGALAdASwgBswBiAMQFEAVAYQAkBtABgF1FQAHAe1jzTy4A7diAAeiAIwB2AKw4AbAE4AzDMUyAHBvkAmZUwAsUgDQgAnog0ScBjTPmyr0h5oC+r06ky4CxEmgBDACcYNGY2JBBuXn4hEXEEaTklVXUtXX0jUwsEHU0cPPtlXQkZHSZFRXl3T3RsHFgwYgw0SBwvbDxBKAAZPFg0EgghMHxBADcuAGtR0IBVRqDYcJFovgFhSISjeRxNZWL5ZUUDQx1FE3NEcsUcE6lFCR0dCXkJbTKakA7cRubWiDtOpYLq9fqDMBBIJcII4DiEAJoZAwgC2OHmi2WrFWPHWcS2iB2ew0B3kRxOZwu2UQ+h0OCkygkyh0GiqBj0ygM7g8IEEXAgcBEPxxMQ28UQAFp5NSEFKvj98EQwCK8ZtQNsdDKtDhlKzjpytPZ5BV5cCcPwUZAAPIAVzQKtiarEhM1V1yTDkxXZr1sZKe6lN3hwAuIrQWkIdYoJCHZMokBl2Gik8g0hkUSc0AZ5Cr+YBakEj+PV13kBj2MiejN17KkDIMceeNl0elLhgM7yztSDufzgJ+oL6A0LToSjLpMgreiZGhrdZlzzpqnOVdOMlUCcD9R7AJwhC4AQFEGH4oQpbpTD0jysFbsE-njL26ketlOSg0Oi52bN27ayACeGII9IjWR0TzPHAL2Od5SneCcZBlZky2UB4DlKLQpAMCtN1wPcD1BY9oxZDQCkeHQHGOGQmAkdRlC1awLlsD9FA9CisO5IA */
+  createMachine({
+    id: 'fetch',
+    initial: 'selected',
+    type: 'parallel',
+    context: {
+      userId: 0,
+      user: undefined,
+      error: undefined,
+      data: [],
+    },
+    states: {
+      idle: {
+        on: {
+          FETCH: { target: 'loading' },
+          target: 'selected',
+        },
+        states: {},
+      },
+      timedOut: {
+        meta: {
+          error: 'Timeout error!',
+        },
+      },
+      deleteUser: {
+        entry: 'getUsers',
+        on: {
+          FETCH: { target: 'selected' },
+          target: 'getUsers',
+        },
+      },
+      selected: {
+        initial: 'fetchingList',
+        states: {
+          fetchingList: {
+            // entry: {},
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-ignore
+            invoke: {
+              id: 'getUsers',
+              src: getUsers,
+              // onDone: 'loaded',
+              after: {
+                5000: 'timedOut',
+              },
+              onDone: {
+                target: 'loaded',
+                actions: assign({
+                  data: (context, event) => {
+                    return event.data;
+                  },
+                }),
+              },
+              onError: {
+                target: 'failed',
+                actions: assign({
+                  data: (context, event) => {
+                    alert(JSON.stringify(context, null, 2));
+                    return event.data;
+                  },
+                }),
+              },
+            },
+          },
+          loaded: {},
+          failed: {
+            on: {
+              // "Event 1": "loading"
+            },
+          },
+        },
+      },
+      loading: {},
+    },
+  });
