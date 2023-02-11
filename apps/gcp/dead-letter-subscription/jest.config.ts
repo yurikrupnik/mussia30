0e77b43f1dc7dc22386ff1892c23dc68c994d8b1
yuri <krupnik.yuri@gmail.com>
2023-02-11 04:24:16 +0200

    checking in

---
+++ apps/gcp/dead-letter-subscription/jest.config.ts
@@ -0,0 +1,16 @@
+/* eslint-disable */
+export default {
+  displayName: 'gcp-dead-letter-subscription',
+  preset: '../../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../../coverage/apps/gcp/dead-letter-subscription',
+};
