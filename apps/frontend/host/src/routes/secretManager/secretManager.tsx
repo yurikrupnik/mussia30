cab35112cee821f56d31653e00b79afb8fa205b7
Yuri Krupnik <krupnik.yuri@gmail.com>
2023-03-06 18:30:47 +0200

    added front end types

--- apps/frontend/host/src/routes/secretManager/secretManager.tsx
+++ apps/frontend/host/src/routes/secretManager/secretManager.tsx
@@ -1,9 +1,18 @@
+enum ContainerRegistryProvider {
+  DOCKER,
+  GCP,
+  AWS,
+  AZURE,
+  GITHUB,
+}
+
 enum SecretManagerProviders {
   GCP_SECRET_MANAGER,
   AWS_SECRET_MANAGER,
   VAULT_SECRET_MANAGER,
   LOCAL_SECRET_MANAGER,
   PULUMI_SECRET_MANAGER,
+  ONE_PASSWORD_SECRET_MANAGER,
 }
 
 const dropDownData = [{}];
