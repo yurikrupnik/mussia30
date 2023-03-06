4fc8290e4476155bfe5855b31c5c10b107cb91f6
Yuri Krupnik <krupnik.yuri@gmail.com>
2023-03-06 12:34:38 +0200

    added new monorepos

--- apps/rust/personal-cli/src/main.rs
+++ apps/rust/personal-cli/src/main.rs
@@ -1,99 +1,38 @@
-use std::collections::BTreeMap;
+use clap::Parser;
+mod gradient;
+// use gradient::{GradientOptions, GradientOptions}
+// use gen_color::{gradient, Cli, Commands};
+use miette::{Context, Result};
+use owo_colors::OwoColorize;
+use rand::Rng;
 
-use clap::{arg, command, ArgGroup, ArgMatches, Command};
+fn main() -> Result<()> {
+  // let cli = Cli::parse();
 
-fn main() {
-  let matches = cli().get_matches();
-  let values = Value::from_matches(&matches);
-  println!("{:#?}", values);
-}
-
-fn cli() -> Command {
-  command!()
-    .group(ArgGroup::new("tests").multiple(true))
-    .next_help_heading("TESTS")
-    .args([
-      arg!(--empty "File is empty and is either a regular file or a directory").group("tests"),
-      arg!(--name <NAME> "Base of file name (the path with the leading directories removed) matches shell pattern pattern").group("tests"),
-    ])
-    .group(ArgGroup::new("operators").multiple(true))
-    .next_help_heading("OPERATORS")
-    .args([
-      arg!(-o - -or "expr2 is not evaluate if exp1 is true").group("operators"),
-      arg!(-a - -and "Same as `expr1 expr1`").group("operators"),
-    ])
-}
-
-#[derive(Clone, PartialEq, Eq, Hash, Debug)]
-pub enum Value {
-  Bool(bool),
-  String(String),
-}
-
-impl Value {
-  pub fn from_matches(matches: &ArgMatches) -> Vec<(clap::Id, Self)> {
-    let mut values = BTreeMap::new();
-    for id in matches.ids() {
-      if matches.try_get_many::<clap::Id>(id.as_str()).is_ok() {
-        // ignore groups
-        continue;
-      }
-      let value_source = matches
-        .value_source(id.as_str())
-        .expect("id came from matches");
-      if value_source != clap::parser::ValueSource::CommandLine {
-        // Any other source just gets tacked on at the end (like default values)
-        continue;
-      }
-      if Self::extract::<String>(matches, id, &mut values) {
-        continue;
-      }
-      if Self::extract::<bool>(matches, id, &mut values) {
-        continue;
-      }
-      unimplemented!("unknown type for {}: {:?}", id, matches);
-    }
-    values.into_values().collect::<Vec<_>>()
-  }
-
-  fn extract<T: Clone + Into<Value> + Send + Sync + 'static>(
-    matches: &ArgMatches,
-    id: &clap::Id,
-    output: &mut BTreeMap<usize, (clap::Id, Self)>,
-  ) -> bool {
-    match matches.try_get_many::<T>(id.as_str()) {
-      Ok(Some(values)) => {
-        for (value, index) in values.zip(
-          matches
-            .indices_of(id.as_str())
-            .expect("id came from matches"),
-        ) {
-          output.insert(index, (id.clone(), value.clone().into()));
-        }
-        true
-      }
-      Ok(None) => {
-        unreachable!("`ids` only reports what is present")
-      }
-      Err(clap::parser::MatchesError::UnknownArgument { .. }) => {
-        unreachable!("id came from matches")
-      }
-      Err(clap::parser::MatchesError::Downcast { .. }) => false,
-      Err(_) => {
-        unreachable!("id came from matches")
-      }
-    }
-  }
-}
-
-impl From<String> for Value {
-  fn from(other: String) -> Self {
-    Self::String(other)
-  }
-}
-
-impl From<bool> for Value {
-  fn from(other: bool) -> Self {
-    Self::Bool(other)
-  }
+  println!("hello word");
+  Ok(())
+  // match &cli.command {
+    // Commands::Gradient(gradient_options) => {
+    //   gradient::generate(gradient_options)
+    //     .wrap_err("when generating gradient")
+    // }
+    // Commands::Random => {
+    //   let mut rng = rand::thread_rng();
+    //
+    //   let color = owo_colors::Rgb(
+    //     rng.gen_range(0..255),
+    //     rng.gen_range(0..255),
+    //     rng.gen_range(0..255),
+    //   );
+    //   let debug_str = "    ";
+    //   print!(
+    //     "{} #{:x}{:x}{:x}",
+    //     debug_str.on_color(color),
+    //     color.0,
+    //     color.1,
+    //     color.2
+    //   );
+    //   Ok(())
+    // }
+  // }
 }
